726316698455e1d9d077c1f515e651010eb6bfd0
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-01-06 10:44:24 -0800

    HoneyMakerBee all tests passing

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,15 @@
-class HoneyMakerBee {
+class HoneyMakerBee extends Bee{
   // TODO..
+  constructor(age, job, color, food, honeyPot, eat) {
+  	super(color, food, eat);
+  	this.age = 10;
+  	this.job = 'make honey';
+  	this.honeyPot = 0;
+  }
+  makeHoney() {
+  	this.honeyPot += 1;
+  }
+  giveHoney() {
+  	this.honeyPot -= 1;
+  }
 };
