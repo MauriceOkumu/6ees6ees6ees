affa28500278e3ca68fe7d601e3d8750f54595d6
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-01-06 10:51:31 -0800

    ForagerBee all tests passing

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,13 @@
-class ForagerBee {
+class ForagerBee extends Bee{
   // TODO..
+  constructor(age, job, color, food, eat, canFly, treasureChest, forage) {
+  	super(color, food, eat);
+  	this.age = 10;
+  	this.job = 'find pollen';
+  	this.canFly = true;
+  	this.treasureChest = [];
+  }
+  forage(treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
