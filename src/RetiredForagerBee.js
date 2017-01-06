cf8ed06b077b413e5ba1c8ff4ba1351c0b2be273
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-01-06 11:06:31 -0800

    RetiredForagerBee specs passing

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,17 @@
-class RetiredForagerBee {
+class RetiredForagerBee extends ForagerBee{
   // TODO..
+  constructor(age, job, color, food, eat, canFly, treasureChest, forage) {
+  	super(color, food, eat, treasureChest);
+  	this.age = 40;
+  	this.job = 'gamble';
+  	this.canFly = false;
+  	this.color = 'grey';
+  	this.treasureChest = [];
+  }
+  gamble(treasure) {
+    this.treasureChest.push(treasure);
+  }
+  forage() {
+  	return 'I am too old, let me play cards instead'
+  }
 };
